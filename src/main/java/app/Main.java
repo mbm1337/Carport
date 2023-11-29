1bb743aa56b0f7d033c324f94f8f6d44303aded4
mbm1337 <mikkelsen96@live.dk>
2023-11-29 14:38:22 +0100

    Dropdowns made
    Not dynamic yet

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,10 @@
 package app;
 
+import app.config.ThymeleafConfig;
+import app.controllers.CarportController;
 import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
     private static final String USER = "postgres";
@@ -11,6 +15,18 @@ public class Main {
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args)  {
+// Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            JavalinThymeleaf.init(ThymeleafConfig.templateEngine());
+        }).start(7070);
+
+        // Routing
+        app.get("/", ctx -> CarportController.carportDropdowns(ctx, connectionPool));
+
+
+
 
 
     }
--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -6,7 +6,51 @@ import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
+
 public class CarportController {
+
+    public static void carportDropdowns(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        //TODO: Make dropdowns dynamic
+
+        ArrayList<Integer> width = new ArrayList<>();
+        width.add(240);
+        width.add(300);
+        width.add(360);
+        width.add(420);
+        width.add(480);
+        ArrayList<Integer> length = new ArrayList<>();
+        length.add(240);
+        length.add(300);
+        length.add(360);
+        length.add(420);
+        length.add(480);
+        ArrayList<String> roof = new ArrayList<>();
+        roof.add("Uden tagplader");
+        roof.add("Plasttrapezplader");
+        ArrayList<Integer> shedWidth = new ArrayList<>();
+        shedWidth.add(240);
+        shedWidth.add(300);
+        shedWidth.add(360);
+        shedWidth.add(420);
+        shedWidth.add(480);
+        ArrayList<Integer> shedLength = new ArrayList<>();
+        shedLength.add(240);
+        shedLength.add(300);
+        shedLength.add(360);
+        shedLength.add(420);
+        shedLength.add(480);
+
+
+        ctx.attribute("carportWidth", width);
+        ctx.attribute("carportLength", length);
+        ctx.attribute("roof", roof);
+        ctx.attribute("shedWidth", shedWidth);
+        ctx.attribute("shedLength", shedLength);
+
+        ctx.render("index.html");
+
+    }
 
     public static void makeCarport(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         if (ctx.formParam("shedWidth").equals("Uden redskabsrum") || ctx.formParam("shedLength").equals("Uden redskabsrum")) {
@@ -46,8 +90,6 @@ public class CarportController {
             Carport carport = new Carport(width, length, height, true, shed);
             ctx.sessionAttribute("carport", carport);
         }
-
-
     }
 
 }

31678a8e46640cc12924479f8134069b2f7f101b
altinkaya <124771353+altinkaya@users.noreply.github.com>
2023-12-09 23:51:41 +0100

    igang med html/css nåed en del ps jeg har lavet siterip på fogs side og bruger mange af ders classer så det skal vi også skrive ind i raporten

---
+++ src/main/resources/templates/siterip/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    navMenu.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+}
